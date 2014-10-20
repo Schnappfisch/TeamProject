3cc1d5c11293acc38709846e9b3456f6a1975c48
Kevin Frei <kefrei@mail.uni-mannheim.de>
2014-10-20 11:05:40 +0200

    Initial Commit

---
+++ Workshop_Prototype/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Workshop_Prototype
+//
+//  Created by Kevin Frei on 20.10.14.
+//  Copyright (c) 2014 Team Project. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
